41df5e62564d024ed4317016d1500ffd029ba5ea
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-05-27 13:09:22 -0700

    Update requirements and parsing in setup.py (#2)
    * Reorg requirements
    
    * Fix case sensitivity in requirements parsing

--- setup.py
+++ setup.py
@@ -15,6 +15,25 @@
 # limitations under the License.
 
 from setuptools import setup, find_packages
+from os import getenv, path
+
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
+
+    for i in range(0, len(requirements)):
+        r = requirements[i]
+        if "@" in r:
+            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
+            r = "@".join(parts)
+        if getenv("GITHUB_TOKEN"):
+            if "github.com" in r:
+                requirements[i] = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
+    return requirements
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -27,16 +46,13 @@ with open("./version.py", "r", encoding="utf-8") as v:
             else:
                 version = line.split("'")[1]
 
-with open("./requirements.txt", "r", encoding="utf-8") as r:
-    requirements = r.readlines()
-
 setup(
     name='neon-enclosure',
     version=version,
     packages=find_packages(),
     url='https://github.com/NeonGeckoCom/neon-enclosure',
     license='NeonAI License v1.0',
-    install_requires=requirements,
+    install_requires=get_requirements("requirements.txt"),
     author='Neongecko',
     author_email='developers@neon.ai',
     description=long_description,
